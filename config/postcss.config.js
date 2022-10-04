27996baeb1c4469efac166ea60efe6353a1cc25d
Yaser Ahmady <y@yaserahmady.com>
2022-10-04 13:34:49 +0200

    Init

---
+++ config/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {
+      config: "./config/tailwind.config.js",
+    },
+    autoprefixer: {},
+  },
+};
